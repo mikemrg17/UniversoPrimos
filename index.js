97828510d5b6e3571d5ddb1d0ab2aec4d66473dd
mikemrg17 <miguelangelmurga1234@gmail.com>
2021-01-28 07:43:18 -0600

    Terminado

--- index.js
+++ index.js
@@ -43,27 +43,27 @@ function menu2(){
 
 function manual(){
     fs.createWriteStream('primos.txt');
+    fs.createWriteStream('datosGraficos.txt');
     fs.appendFileSync('primos.txt', "U={\n", (error)=>{
         if (error) console.log(`Error: ${error}`);
     });
     rl.question('Por favor inserta un límite ', (limite) =>{
         let numeroDecimal;
-        let banderaProceso;
         let numeroPrimo;
+        let contadorUnos;
         for(numeroDecimal = 0; numeroDecimal < limite; numeroDecimal++){
             if(esPrimo(numeroDecimal)){
                 numeroPrimo = numeroDecimal.toString(2);
-                fs.appendFileSync('primos.txt',"\tNúmero primo: " +numeroDecimal + "-> Binario: " + numeroPrimo +"\n", (error)=>{
+                contadorUnos = contarUnos(numeroPrimo);
+                fs.appendFileSync('primos.txt',"\tNúmero primo: " +numeroDecimal + "-> Binario: " + numeroPrimo +" -> No. de Unos: " + contadorUnos +"\n", (error)=>{
                     if (error) console.log(`Error: ${error}`);
                 });
+                let logaritmo2 = Math.log2(contadorUnos);
+                let logaritmo10 = Math.log10(contadorUnos);
+                fs.appendFileSync('datosGraficos.txt',"Numero primo: " + numeroDecimal + " ->Numeros de unos:" + contadorUnos + "->Log. 2 : " + logaritmo2 + " -> Log. 10:" + logaritmo10 + "\n", (error)=>{
+                if(error) console.log(`Error: ${error}`);
+            });
             }
-            /*
-            banderaProceso = proceso(numeroBinario, potencia);
-            if(banderaProceso == 1){
-                break;
-            }else{
-                continue;
-            }*/
         }
         fs.appendFile('primos.txt', "}", (error)=>{
             if (error) console.log(`Error: ${error}`);
@@ -75,19 +75,26 @@ function manual(){
 
 function aleatoria(){
     fs.createWriteStream('primos.txt');
+    fs.createWriteStream('datosGraficos.txt');
     fs.appendFileSync('primos.txt', "U={\n", (error)=>{
         if (error) console.log(`Error: ${error}`);
     });
-    let limite = Math.round(Math.random() * (100-0) + 0);
+    let limite = Math.round(Math.random() * (1000000-0) + 0);
     console.log(`Límite: ${limite}`);
     let numeroDecimal;
     let numeroPrimo;
-    //let banderaProceso;
+    let contadorUnos;
     for(numeroDecimal = 0; numeroDecimal < limite; numeroDecimal++){
         if(esPrimo(numeroDecimal)){
             numeroPrimo = numeroDecimal.toString(2);
-            fs.appendFileSync('primos.txt',"\tNúmero primo: " +numeroDecimal + "-> Binario: " + numeroPrimo +"\n", (error)=>{
+            contadorUnos = contarUnos(numeroPrimo);
+            fs.appendFileSync('primos.txt',"\tNúmero primo: " +numeroDecimal + "-> Binario: " + numeroPrimo +" -> No. de Unos: " + contadorUnos +"\n", (error)=>{
                 if (error) console.log(`Error: ${error}`);
+            });
+            let logaritmo2 = Math.log2(contadorUnos);
+            let logaritmo10 = Math.log10(contadorUnos);
+            fs.appendFileSync('datosGraficos.txt',"Numero primo: " + numeroDecimal +  "->Numeros de unos:" + contadorUnos + "->Log. 2 : " + logaritmo2 + " -> Log. 10:" + logaritmo10 + "\n", (error)=>{
+                if(error) console.log(`Error: ${error}`);
             });
         }
     }
@@ -105,3 +112,16 @@ function esPrimo(numeroDecimal){
 	// Si no se pudo dividir por ninguno de los de arriba, sí es primo
 	return true;
 }
+
+
+function contarUnos(numeroPrimo){
+    let contador = 0;
+    for(let i = 0; i < numeroPrimo.length; i++){
+        if(numeroPrimo[i] == "1"){
+            contador++;
+        }
+    }
+    return contador;
+}
+
+
